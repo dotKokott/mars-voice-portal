6d3f89371559979336d506ba3649fc8e8abda067
Jonas Johansson <jns.johansson@gmail.com>
2020-03-19 21:43:45 -0700

    generatio radio buttons per screen, record is toggle and sends selected screen coords

--- public/js/phone.js
+++ public/js/phone.js
@@ -1,4 +1,6 @@
-const $ = require("jquery");
+/** @format */
+
+const $ = require('jquery');
 const recordrtc = require('recordrtc');
 
 const mediaConstraints = { video: true, audio: true };
@@ -8,98 +10,119 @@ let rtc;
 
 let currentDataURL;
 
-navigator.mediaDevices.getUserMedia(mediaConstraints).then((result) => { userStream = result })
+navigator.mediaDevices.getUserMedia(mediaConstraints).then(result => {
+  userStream = result;
 
 var socket = io();
 
-socket.on('receiveAvailableScreens', (data) => {
-    data.ids.forEach((item) => {
-        $('#listScreens').append(
-            $('<option></option>').val(item).html(item)
-        );
-    })
-})
+socket.on('receiveAvailableScreens', data => {
+  console.log(data);
+  var i = 0;
+  // document.getElementById('listScreens').innerHTML = '';
+  for (let screen of data.screens) {
+    var btnInput = document.createElement('input');
+    var btnLabel = document.createElement('label');
+    if (i == 0) {
+      btnInput.checked = true;
+    }
+    btnLabel.setAttribute('data-x', screen.x);
+    btnLabel.setAttribute('data-y', screen.y);
+    var id = `screen-${i++}`;
+    btnInput.type = 'radio';
+    btnInput.id = id;
+    btnInput.name = 'screen';
+    btnLabel.setAttribute('for', id);
+    document.getElementById('listScreens').appendChild(btnInput);
+    document.getElementById('listScreens').appendChild(btnLabel);
+    /*$('#listScreens').append(
+      $('<option></option>')
+        .val(item)
+        .html(item)
+    );*/
+  }
+});
 
 var videoPreview = $('#video_preview')[0];
 
 function init() {
-    console.log("Initializing...");
-    socket.emit('getAvailableScreens');
+  console.log('Initializing...');
+  socket.emit('getAvailableScreens');
 }
 
 let firstTime = true;
 function startRecording() {
-    var options = {
-        recorderType: recordrtc.StereoAudioRecorder,
-        mimeType: 'audio/webm', // or video/webm\;codecs=h264 or video/webm\;codecs=vp9
-        audioBitsPerSecond: 128000,
-        videoBitsPerSecond: 128000,
-        bitsPerSecond: 128000 // if this line is provided, skip above two
-    };
-    
-    rtc = recordrtc(userStream, options);        
+  var options = {
+    recorderType: recordrtc.StereoAudioRecorder,
+    mimeType: 'audio/webm', // or video/webm\;codecs=h264 or video/webm\;codecs=vp9
+    audioBitsPerSecond: 128000,
+    videoBitsPerSecond: 128000,
+    bitsPerSecond: 128000 // if this line is provided, skip above two
+  };
 
-    rtc.startRecording();    
+  rtc = recordrtc(userStream, options);
+
+  rtc.startRecording();
 }
 
 function stopRecording() {
-    rtc.stopRecording((blobUrl) => {
-        console.log("Stopped");
-        rtc.getDataURL((dataURL) => {
-            currentDataURL = dataURL;
-
-            sendToScreen(0, 0);
-        })
+  rtc.stopRecording(blobUrl => {
+    console.log('Stopped');
+    rtc.getDataURL(dataURL => {
+      currentDataURL = dataURL;
+      var selectedScreen = document.querySelector('input[name="screen"]:checked');
+      var x = selectedScreen.getAttribute('data-x');
+      var y = selectedScreen.getAttribute('data-y');
+      sendToScreen(x, y);
     });
-
+  });
 }
 
 function sendToScreen(x, y) {
-    socket.emit('sendVideo', { x: x, y:y , video: currentDataURL });
-    
-    location.reload();
+  socket.emit('sendVideo', { x: x, y: y, video: currentDataURL });
+
+  location.reload();
 }
 
-$( document ).ready(() => {
-    init();
-})
+$(document).ready(() => {
+  init();
+});
 
 // $( document ).click(() => {
 //     console.log($('#listScreens').val())
 // })
 
-// $( document ).mousedown(() => {
-//     startRecording();
-// })
+$('#btnRecord').mousedown(() => {
+  startRecording();
+});
 
-// $( document ).mouseup(() => {
-//     stopRecording();
-// })
+$('#btnRecord').mouseup(() => {
+  stopRecording();
+});
 var recording = false;
 
-$('#btnRecord').click(() => {
-    if(!recording) {
-        recording = true;
-        startRecording();
+// $('#btnRecord').click(() => {
+//   if (!recording) {
+//     recording = true;
+//     startRecording();
 
-        return;
-    }
+//     return;
+//   }
 
-    if(recording) {
-        recording = false;
-        stopRecording();
+//   if (recording) {
+//     recording = false;
+//     stopRecording();
 
-        return;
-    }
-})
+//     return;
+//   }
+// });
 
 $('#btnSend').click(() => {
-    var screen = $('#listScreens').val()
-    
-    // if (screen === '') {
-    //     alert('Please choose a screen!')
-    //     return
-    // }
+  var screen = $('#listScreens').val();
 
-    sendToScreen(parseInt(screen));
-})
+  // if (screen === '') {
+  //     alert('Please choose a screen!')
+  //     return
+  // }
+
+  sendToScreen(parseInt(screen));
+});

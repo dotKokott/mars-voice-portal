d2ea86af25d44f626548255f02848c5ebb460199
Christian Kokott <christian.kokott@gmail.com>
2020-03-20 14:13:46 -0700

    reintegrated p5 into screen.js/html

--- public/js/screen.js
+++ public/js/screen.js
@@ -37,3 +37,30 @@ socket.on('playVideo', (data) => {
     video.play();
 })
 
+const WIDTH = 1280;
+const HEIGHT = 720;
+
+var canvas = new p5((p) => {
+    let now = Date.now();
+    let then = Date.now();
+    const INTERVAL_60 = 0.0166666; //60 fps
+
+    p.setup = () => {
+        p.colorMode(p.HSL);
+
+        var c = p.createCanvas(WIDTH, HEIGHT);
+        c.canvas.style.width = "100%";
+        c.canvas.style.height = "100%";
+
+        console.log("setup")
+    }
+
+    p.draw = () => {
+        then = now;
+        now = Date.now();
+        
+        p.background(0, 0, 0, 360);
+
+        p.ellipse(50, 50, 80, 80);
+    }
+})
